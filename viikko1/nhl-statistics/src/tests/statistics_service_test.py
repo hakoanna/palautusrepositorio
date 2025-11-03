b1d75a717718ac935d1797a2f8d5f022545d45c9
Anna Hakosalo <anna.hakosalo@outlook.com>
2025-11-03 16:54:16 +0200

    nothing is working why

--- viikko1/nhl-statistics/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics/src/tests/statistics_service_test.py
@@ -19,19 +19,33 @@ class TestStatisticsService(unittest.TestCase):
             PlayerReaderStub()
         )
 
-    def test_pelaaja_loytyy(self):
-        for player in self.stats._players:
-            if "Kurri" in player.name:
-                palautus = player
-                
-        self.assertAlmostEqual(str(palautus), "Kurri EDM 37 + 53 = 90")
+    def test_konstruktori_luo_pelaajatiedot_oikein(self):
+        players = [str(player) for player in self.stats._players]
+        real_players = [
+            "Semenko EDM 4 + 12 = 16",
+            "Lemieux PIT 45 + 54 = 99",
+            "Kurri EDM 37 + 53 = 90",
+            "Yzerman DET 42 + 56 = 98",
+            "Gretzky EDM 35 + 89 = 124"
+        ]
 
-    def test_pelaajaa_ei_loydy(self):
-        loytyy = None
-        for player in self.stats._players:
-            if "Turri" in player.name:
-                loytyy = True
-                
-        self.assertAlmostEqual(loytyy, None)
+        self.assertEqual(players, real_players)
 
-    
+    def test_haettu_pelaaja_loytyy(self):
+        result = None
+        for player in self.stats._players:
+            if "Gretzky" in player.name:
+                result = player
+
+        real_result = self.stats._players[4]
+
+        self.assertEqual(result, real_result)
+
+    def test_haettua_pelaajaa_ei_ole(self):
+        result = None
+        for player in self.stats._players:
+            if "Bretzky" in player.name:
+                result = player
+
+        self.assertIsNone(result)
+
