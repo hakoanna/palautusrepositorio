b9a79c2cb73c78c2179975a862ff47baa0bfe5b5
Anna Hakosalo <anna.hakosalo@outlook.com>
2025-11-04 15:50:29 +0200

    wk2 ex2

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,16 +5,20 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
         player = Player(player_dict)
         players.append(player)
 
-    print("Oliot:")
+
+    nationality = "FIN"
+    print(f"Players from {nationality}:")
+    print()
 
     for player in players:
-        print(player)
+        if player.nation == nationality:
+            print(player)
+
+if __name__ == '__main__':
+    main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,9 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nation = dict['nationality']
+        self.team = dict['team']
+        self.games = dict['games']
     
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.nation}, {self.team}, {self.games}"
