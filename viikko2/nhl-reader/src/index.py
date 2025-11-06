2184acf2e7b4d28241af9f271c7bb1c60f5959da
Anna Hakosalo <anna.hakosalo@outlook.com>
2025-11-06 17:18:50 +0200

    wk2 ex4

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,26 +1,16 @@
-import requests
-from player import Player
+from player import PlayerReader, PlayerStats
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    print(reader)
+    stats = PlayerStats(reader)
+    print(stats)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-
-    nationality = "FIN"
-    print(f"Players from {nationality}:")
-    print()
-
-    players.sort(key=lambda player: player.points)
-    
-    for player in reversed(players):
-        if player.nation == nationality:
-            print(player)
+    for player in players:
+        print(player)
 
 if __name__ == '__main__':
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,7 +1,9 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-        self.nation = dict['nationality']
+        self.nationality = dict['nationality']
         self.team = dict['team']
         self.games = dict['games']
         self.goals = dict['goals']
@@ -10,3 +12,32 @@ class Player:
     
     def __str__(self):
         return f"{self.name:20} {self.team:15} {self.goals} + {self.assists} = {self.goals + self.assists}"
+    
+
+class PlayerReader:
+    def __init__(self, url):
+        self.all_players = requests.get(url).json()
+
+    def get_players(self):
+        self.players = []
+        for player_dict in self.all_players:
+            player = Player(player_dict)
+            self.players.append(player)
+
+        return self.players
+
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        self.players.sort(key=lambda player: player.points)
+
+        result = []
+    
+        for player in reversed(self.players):
+            if player.nationality == nationality:
+                result.append(str(player))
+
+        return result
